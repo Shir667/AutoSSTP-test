dc21d007b3d3c3ced3e5f4fe8552c01044fd6351
Shirli Begmyradow <shrilibegmyradow@gmail.com>
2025-08-01 11:41:12 +0500

    Fix: Generate valid IPA for iOS

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -17,21 +17,3 @@ android {
     compileSdk = 34
     defaultConfig { minSdk = 21 }
 }
-
-
-tasks.register("packForXcode") {
-    group = "build"
-    // DEĞİŞTİR: Debug yerine RELEASE framework'ü kopyala
-    dependsOn("linkReleaseFrameworkIosArm64")  // <-- BU SATIRI DEĞİŞTİRİN
-
-    doLast {
-        val fromDir = buildDir.resolve("bin/iosArm64/releaseFramework")  // <-- releaseFramework
-        val toDir   = buildDir.resolve("xcode-frameworks")
-
-        if (fromDir.exists()) {
-            fromDir.copyRecursively(toDir, overwrite = true)
-        } else {
-            throw GradleException("Framework klasörü bulunamadı: $fromDir")
-        }
-    }
-}
